eb6419352023791fb0ed4816edf3b57480567364
amila941023 <amivishcreations94@gmail.com>
2025-06-11 15:19:52 +0530

    Created DashboardCustomer java file in gui model

---
+++ src/gui/DashboardCustomer.java
@@ -0,0 +1,35 @@
+
+package gui;
+
+import models.User;
+import javax.swing.*;
+
+/**
+ *
+ * @author Amila_Vishwajith
+ */
+public class DashboardCustomer extends JFrame 
+{
+    public DashboardCustomer(User user) 
+    {
+        setTitle("Customer Dashboard");
+        setSize(400, 300);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setLayout(new BoxLayout(getContentPane(), BoxLayout.Y_AXIS));
+
+        add(new JLabel("👋 Welcome, " + user.getUsername()));
+        add(new JButton("View Flights"));
+        add(new JButton("Book Flight"));
+        add(new JButton("My Bookings"));
+        add(new JButton("Logout")).addActionListener(e -> 
+        {
+            new LoginForm();
+            dispose();
+        });
+
+        setVisible(true);
+    }
+}
+   
+    
