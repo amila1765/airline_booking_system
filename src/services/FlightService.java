d16bed2e58d56e537f94007e7104e05fd8623109
amila941023 <amivishcreations94@gmail.com>
2025-06-21 03:26:49 +0530

    Added transit flight option to flight service form

--- src/services/FlightService.java
+++ src/services/FlightService.java
@@ -22,10 +22,8 @@ public class FlightService
         try (Connection conn = DBConnection.getInstance().getConnection();
              PreparedStatement stmt = conn.prepareStatement(sql)) 
         {
-
             stmt.setInt(1, originId);
             stmt.setInt(2, destinationId);
-
             ResultSet rs = stmt.executeQuery();
 
             while (rs.next()) 
@@ -49,4 +47,78 @@ public class FlightService
 
         return flights;
     }
+        
+
+        public static List<List<Flight>> getTransitFlights(int originId, int destinationId)
+        {
+        List<List<Flight>> transitOptions = new ArrayList<>();
+
+        String sqlroute1 = "SELECT * FROM flights WHERE origin_airport_id = ?";
+        String sqlroute2 = "SELECT * FROM flights WHERE destination_airport_id = ?";
+
+        try (Connection conn = DBConnection.getInstance().getConnection())
+        {
+            //Get all flights from origin
+            PreparedStatement stmt1 = conn.prepareStatement(sqlroute1);
+            stmt1.setInt(1, originId);
+            ResultSet rs1 = stmt1.executeQuery();
+
+            while (rs1.next()) 
+            {
+                Flight route1 = new Flight
+                    (
+                    rs1.getInt("flight_id"),
+                    rs1.getInt("airplane_id"),
+                    rs1.getInt("origin_airport_id"),
+                    rs1.getInt("destination_airport_id"),
+                    rs1.getTimestamp("departure_time").toLocalDateTime(),
+                    rs1.getTimestamp("arrival_time").toLocalDateTime()
+                    );
+
+            //For each route1, try to find a matching route2 flight
+            PreparedStatement stmt2 = conn.prepareStatement(sqlroute2);
+            stmt2.setInt(1, destinationId);
+            ResultSet rs2 = stmt2.executeQuery();
+
+            while (rs2.next()) 
+            {
+                int transitStart = rs2.getInt("origin_airport_id");
+                LocalDateTime route2Dep = rs2.getTimestamp("departure_time").toLocalDateTime();
+
+                if (transitStart == route1.getArrivalAirportId()
+                        && route1.getArrivalTime().plusHours(1).isBefore(route2Dep)) 
+                {
+
+                    Flight route2 = new Flight
+                    (
+                            rs2.getInt("flight_id"),
+                            rs2.getInt("airplane_id"),
+                            rs2.getInt("origin_airport_id"),
+                            rs2.getInt("destination_airport_id"),
+                            route2Dep,
+                            rs2.getTimestamp("arrival_time").toLocalDateTime()
+                    );
+
+                    List<Flight> option = new ArrayList<>();
+                    option.add(route1);
+                    option.add(route2);
+                    transitOptions.add(option);
+                }
+            }
+            rs2.close();
+            stmt2.close();
+            }
+
+            rs1.close();
+            stmt1.close();
+
+        }   
+        catch (SQLException e)
+        {
+        e.printStackTrace();
+        }
+
+        return transitOptions;
+    }
+
 }
